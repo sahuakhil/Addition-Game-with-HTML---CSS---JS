b6326c1fc59e5d6a8970ee45441f15a6ea99a3e4
Akhilesh <akhileshshah75@gmail.com>
2023-08-08 09:30:25 +0530

    Addition-Game

---
+++ app.js
@@ -0,0 +1,35 @@
+let firstNumber = document.getElementById("firstNumber");
+let secondNumber = document.getElementById("secondNumber");
+let userInput = document.getElementById("userInput");
+let resultOfGame = document.getElementById("gameResult");
+
+function restartButtonFunction() {
+    let firstRandomNumber = Math.random() * 100;
+    firstRandomNumber = Math.ceil(firstRandomNumber);
+    firstNumber.textContent = firstRandomNumber;
+
+    let secondRandomNumber = Math.random() * 100;
+    secondRandomNumber = Math.ceil(secondRandomNumber);
+    secondNumber.textContent = secondRandomNumber;
+
+    resultOfGame.textContent = "";
+    userInput.value = "";
+}
+
+restartButtonFunction();
+
+function checkButtonFunction() {
+    let firstRandomNumber = parseInt(firstNumber.textContent);
+    let secondRandomNumber = parseInt(secondNumber.textContent);
+    let input = parseInt(userInput.value);
+
+    let sumOfNumbers = firstRandomNumber + secondRandomNumber;
+
+    if (sumOfNumbers === input) {
+        resultOfGame.textContent = "Congratulation! You got it right.";
+        resultOfGame.style.backgroundColor = "#028a0f";
+    } else {
+        resultOfGame.textContent = "Please Try Again!";
+        resultOfGame.style.backgroundColor = "#1e217c";
+    }
+}
